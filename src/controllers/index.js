21affed769b8cdba9ed65340bba6fb4966db7736
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-25 13:54:39 +0300

    better error handling for controller layer

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,12 @@
 const { listTotalCounts } = require('../services');
 
 const httpStatus = require('http-status');
+const ApiError = require('../errors/ApiError');
 
 const getTotalCounts = async (req, res, next) => {
+  // destructure request body
   const { startDate, endDate, minCount, maxCount } = req.body;
-
+  
   try {
     const response = await listTotalCounts(
       startDate,
@@ -12,11 +14,22 @@ const getTotalCounts = async (req, res, next) => {
       minCount,
       maxCount
     );
+
+    if (!response)
+      return res
+        .status(httpStatus.NOT_FOUND)
+        .send({ code: 1, msg: 'Not Found' });
+
     res
       .status(httpStatus.OK)
       .send({ code: 0, msg: 'Success', records: response });
   } catch (err) {
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.message);
+    next(
+      new ApiError({
+        code: 3,
+        msg: 'Controllers.getTotalCounts: ' + err.message,
+      })
+    );
   }
 };
 
