27d367cddefc362c117ab0efed9819222466c001
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-25 11:00:35 +0300

    implement async await at controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,14 +2,13 @@ const { listTotalCounts } = require('../services');
 
 const httpStatus = require('http-status');
 
-const getTotalCounts = (req, res, next) => {
-  listTotalCounts()
-    .then((response) => {
-      res.status(httpStatus.OK).send(response);
-    })
-    .catch((e) => {
-      res.status(httpStatus.INTERNAL_SERVER_ERROR).send(e.message);
-    });
+const getTotalCounts = async (req, res, next) => {
+  try {
+    const response = await listTotalCounts();
+    res.status(httpStatus.OK).send(response);
+  } catch (err) {
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.message);
+  }
 };
 
 module.exports = { getTotalCounts };
