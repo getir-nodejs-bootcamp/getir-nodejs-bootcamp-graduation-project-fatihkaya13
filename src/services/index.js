e5a28718c8675587e5ce19f5f8d0ee60f90f0a72
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-25 10:50:11 +0300

    create service layer

---
+++ src/services/index.js
@@ -0,0 +1,40 @@
+const Record = require('../models/Records');
+
+const listTotalCounts = () => {
+  const pipeline = [
+    {
+      $match: {
+        createdAt: {
+          $gte: new Date('2016-01-26'), // startDate
+          $lte: new Date('2018-02-02'), // endDate
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        key: 1,
+        createdAt: 1,
+        totalCount: {
+          $reduce: {
+            input: '$counts',
+            initialValue: 0,
+            in: { $add: ['$$value', '$$this'] },
+          },
+        },
+      },
+    },
+    {
+      $match: {
+        totalCount: {
+          $gte: 2800, // minCount
+          $lte: 2900, // maxCount
+        },
+      },
+    },
+  ];
+
+  return Record.aggregate(pipeline);
+};
+
+module.exports = { listTotalCounts };
