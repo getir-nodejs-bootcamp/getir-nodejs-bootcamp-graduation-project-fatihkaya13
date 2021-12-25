a220a7b12830361a8a81cfd8a36469401e95d30a
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-25 11:11:58 +0300

    create parametric function at controllers

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,9 +3,18 @@ const { listTotalCounts } = require('../services');
 const httpStatus = require('http-status');
 
 const getTotalCounts = async (req, res, next) => {
+  const { startDate, endDate, minCount, maxCount } = req.body;
+
   try {
-    const response = await listTotalCounts();
-    res.status(httpStatus.OK).send(response);
+    const response = await listTotalCounts(
+      startDate,
+      endDate,
+      minCount,
+      maxCount
+    );
+    res
+      .status(httpStatus.OK)
+      .send({ code: 0, msg: 'Success', records: response });
   } catch (err) {
     res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err.message);
   }
