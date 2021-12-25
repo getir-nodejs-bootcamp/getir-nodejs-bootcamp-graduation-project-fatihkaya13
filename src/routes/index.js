2cfcd0f5f10ad7b1ddb980db3b9c7f5376dacfce
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-25 13:55:26 +0300

    add validation for the route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const validate = require('../middlewares/validate');
+const getTotalCountsValidation = require('../validations/Records');
+
 const { getTotalCounts } = require('../controllers');
 
-router.post('/', getTotalCounts);
+// validate the request body then call the function getTotalCounts from controller layer
+router.post('/', validate(getTotalCountsValidation), getTotalCounts);
 
 module.exports.RecordsRoutes = router;
-
