59a80dc6540f2a2ef66ccd18ccd28c4549e13c2b
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-25 11:12:06 +0300

    pass parameters to services

--- src/services/index.js
+++ src/services/index.js
@@ -1,12 +1,12 @@
 const Record = require('../models/Records');
 
-const listTotalCounts = async () => {
+const listTotalCounts = async (startDate, endDate, minCount, maxCount) => {
   const pipeline = [
     {
       $match: {
         createdAt: {
-          $gte: new Date('2016-01-26'), // startDate
-          $lte: new Date('2018-02-02'), // endDate
+          $gte: new Date(startDate),
+          $lte: new Date(endDate),
         },
       },
     },
@@ -27,8 +27,8 @@ const listTotalCounts = async () => {
     {
       $match: {
         totalCount: {
-          $gte: 2800, // minCount
-          $lte: 2900, // maxCount
+          $gte: minCount,
+          $lte: maxCount,
         },
       },
     },
