6e7a44dacfb068a085b5fc9472fec02990ada38f
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-25 11:00:47 +0300

    comment unnecessary fields

--- src/models/Records.js
+++ src/models/Records.js
@@ -1,9 +1,10 @@
 const Mongoose = require('mongoose');
 
 const RecordSchema = new Mongoose.Schema({
-  key: String,
-  createdAt: Date,
-  counts: [Number],
+  // field definitions are not necessary here
+  // key: String,
+  // createdAt: Date,
+  // counts: [Number],
 });
 
 const Record = Mongoose.model('record', RecordSchema);
