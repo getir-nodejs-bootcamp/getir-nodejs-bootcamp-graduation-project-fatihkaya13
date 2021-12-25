dc8b386dafe58a3c59b81833b30000df604f1152
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-25 13:55:38 +0300

    comments for query

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,12 @@
 const Record = require('../models/Records');
+const ApiError = require('../errors/ApiError');
 
 const listTotalCounts = async (startDate, endDate, minCount, maxCount) => {
+  // the pipeline is composed of two match stages and one project stage
+  // it is unnecessary to calculate the sum of counts arrays for each document
+  // first filter with given date criteria for better performance
+  // then calculate the sum of counts for each array
+  // finally filter with given min-max count criteria
   const pipeline = [
     {
       $match: {
@@ -34,7 +40,14 @@ const listTotalCounts = async (startDate, endDate, minCount, maxCount) => {
     },
   ];
 
-  return await Record.aggregate(pipeline);
+  try {
+    return await Record.aggregate(pipeline);
+  } catch (err) {
+    return new ApiError({
+      code: 3,
+      msg: 'Services.listTotalCounts: ' + err.message,
+    });
+  }
 };
 
 module.exports = { listTotalCounts };
