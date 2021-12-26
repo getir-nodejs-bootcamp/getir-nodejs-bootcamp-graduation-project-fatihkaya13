8f31cb40629a23e9810a12abb7c1339bf704e1ec
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-26 23:43:22 +0300

    refactoring and adding logger to error middleware

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,7 +1,19 @@
+const logger = require('../logger');
+
 module.exports = (error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message || 'Internal Server Error....';
+  logger.log({
+    level: 'error',
+    message: {
+      status: status,
+      message: message,
+    },
+  });
+
   // if not an error code is specified, the default is set to 500
-  res.status(error.status || 500);
+  res.status(status);
   res.json({
-    error: error.message || 'Internal Server Error....',
+    message,
   });
 };
